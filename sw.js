b1bc2103ca4b765917fb6ad0742896929c5a07e7
yokoliz10 <yokoliz10@outlook.com>
2026-05-04 10:43:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,9 @@
-self.addEventListener('install', (e) => console.log('[GemiLi] 空军地勤人员就位'));
-self.addEventListener('fetch', (e) => {});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
+
+self.addEventListener('fetch', (e) => {
+    // 军师军令：彻底废除缓存机制，保证主公每次看到的都是最新版！
+    e.respondWith(
+        fetch(e.request, { cache: 'no-store' }).catch(() => new Response('Network Error'))
+    );
+});
